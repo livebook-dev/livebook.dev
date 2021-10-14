9f8de3d8147106bcf772d79a42965a0354ae4e11
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-10-14 12:42:06 +0200

    Restructure files

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,28 @@
 import { resolve } from "path";
+import { readdirSync } from "fs";
 import handlebars from "vite-plugin-handlebars";
 
+const root = resolve(__dirname, "src");
+
 export default {
+  root,
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, "partials"),
+      partialDirectory: resolve(root, "partials"),
     }),
   ],
   build: {
     rollupOptions: {
-      input: {
-        index: resolve(__dirname, "index.html"),
-        badge: resolve(__dirname, "badge.html"),
-      },
+      // Generate an input entry for every page
+      input: Object.fromEntries(
+        readdirSync(root)
+          .filter((filename) => filename.endsWith(".html"))
+          .map((filename) => {
+            const key = filename.replace(/\.html$/, "");
+            const path = resolve(root, filename);
+            return [key, path];
+          })
+      ),
     },
   },
 };
