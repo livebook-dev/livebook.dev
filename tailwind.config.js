9c35821b02ac0af2bae2f5ae32d902d80997a5ac
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-06-28 15:00:19 +0100

    Livebook Hub/Enterprise (#38)

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,13 @@ module.exports = {
         },
       },
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   variants: {
     extend: {},
