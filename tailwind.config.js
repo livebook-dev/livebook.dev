5cccfb578ccf7dc4b3ea5435e700bac235110853
paulo-valim <plvalim@gmail.com>
2022-07-02 11:29:58 +0200

    cleaned comments on tw.conf.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,22 +35,11 @@ module.exports = {
     },
     screens: {
       sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
       maxsm: { min: "100px", max: "767px" },
-      // => @media (max-width: 767px) { ... }
-
       md: "768px",
-      // => @media (min-width: 768px) { ... }
-
       lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
   },
 
