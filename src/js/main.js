e27982beee2db4c00365929ecfade0ed079ac5b8
paulo-valim <plvalim@gmail.com>
2022-12-16 19:50:07 -0500

    changes on JS

--- src/js/main.js
+++ src/js/main.js
@@ -323,31 +323,6 @@ if (document.body.dataset.page === "run") {
     }
   }
 
-  // // Show filtered elements
-  // function AddClass(element, name) {
-  //   var i, arr1, arr2;
-  //   arr1 = element.className.split(" ");
-  //   arr2 = name.split(" ");
-  //   for (i = 0; i < arr2.length; i++) {
-  //     if (arr1.indexOf(arr2[i]) == -1) {
-  //       element.className += " " + arr2[i];
-  //     }
-  //   }
-  // }
-
-  // // Hide elements that are not selected
-  // function RemoveClass(element, name) {
-  //   var i, arr1, arr2;
-  //   arr1 = element.className.split(" ");
-  //   arr2 = name.split(" ");
-  //   for (i = 0; i < arr2.length; i++) {
-  //     while (arr1.indexOf(arr2[i]) > -1) {
-  //       arr1.splice(arr1.indexOf(arr2[i]), 1);
-  //     }
-  //   }
-  //   element.className = arr1.join(" ");
-  // }
-
   // Add active class to the current control button (highlight it)
   var btnContainer = document.getElementById("btn-integration");
   var btns = btnContainer.getElementsByClassName("button-integration");
