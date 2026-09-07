f38ad1def8fbda99d85d5c584dd7f4cc2f10d07d
Jonatan Kłosko <jonatanklosko@gmail.com>
2026-09-07 23:23:06 +0200

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
