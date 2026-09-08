c6fd2043dca82e8b059705949b6a85fa6b1b9d42
Jonatan Kłosko <jonatanklosko@gmail.com>
2026-09-08 14:41:32 +0200

    Add tidewave

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import tidewave from "tidewave/vite-plugin";
 
 export default defineConfig({
   markdown: {
     shikiConfig: { theme: "github-dark" },
   },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tidewave(), tailwindcss()],
   },
 });
